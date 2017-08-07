d020a7e9996209e36639ebe584b2e44573c6b6e5
Ne0D3v <acc4develop@gmail.com>
2017-08-07 12:05:08 +0700

    update service.js

--- src/services/service.js
+++ src/services/service.js
@@ -1,29 +1,52 @@
-import { request, config } from '../utils'
-const { apiURL, apiPrefix, api } = config
-const { service } = api
-const idToken = localStorage.getItem('id_token')
+import { request, config, crypt } from 'utils'
 
-export async function queryService (params) {
+const { services } = config.api
+
+export async function query (params) {
+  console.log('query_params', params,services);
+  const apiHeaderToken = crypt.apiheader()
   return request({
-    url: apiURL + apiPrefix + service,
+    url: services,
     method: 'get',
     data: params,
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'JWT ' + idToken
-    }
+    headers: apiHeaderToken,
   })
 }
 
-export async function queryServiceByCode (params) {
+export async function add (params) {
+  console.log('params:', params, services);
+  const url = params.serviceCode ? services + '/' + params.serviceCode : null
+  const apiHeaderToken = crypt.apiheader()
   return request({
-    url: apiURL + apiPrefix + service + '/' + params,
-    method: 'get',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'JWT ' + idToken
-    }
+    url:url,
+    method: 'post',
+    data: params,
+    body: params,
+    headers: apiHeaderToken,
+  })
+}
+
+export async function edit (params) {
+  console.log('edit-params:', params);
+  const url = params.serviceCode ? `${services}/${params.serviceCode}` : serviceCode
+  const apiHeaderToken = crypt.apiheader()
+  return request({
+    url,
+    method: 'put',
+    data: params,
+    body: params,
+    headers: apiHeaderToken,
+  })
+}
+
+export async function remove (params) {
+  console.log('params', params);
+  const url = params.id.serviceCode ? `${services}/${params.id.serviceCode}` : services
+  const apiHeaderToken = crypt.apiheader()
+  return request({
+    url,
+    method: 'delete',
+    data: params.data,
+    headers: apiHeaderToken,
   })
 }
