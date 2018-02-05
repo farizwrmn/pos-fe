84dbc1684d643f315cfa633091ef9e8f156d540c
veirryAu <veirryau@gmail.com>
2018-02-05 08:42:48 +0700

    PurchaseList uncaught validation pattern

--- src/routes/transaction/purchase/edit/PurchaseList.js
+++ src/routes/transaction/purchase/edit/PurchaseList.js
@@ -204,8 +204,6 @@ const PurchaseList = ({ curHead, onChooseItem, transNo, onChangeTotalItem, onVoi
             initialValue: item.total,
             rules: [{
               required: true,
-              pattern: /^([0-9.]{0,13})$/i,
-              message: 'Total is not define',
             }],
           })(<Input disabled />)}
         </FormItem>
