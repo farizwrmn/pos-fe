995276009bb9fd9bdff02385ece9c6eaca7df523
veirryAu <veirryau@gmail.com>
2018-03-05 21:05:51 +0700

    fix: unstaged router directory

--- src/router.js
+++ src/router.js
@@ -345,14 +345,6 @@ const Routers = function ({ history, app }) {
             }, 'report-product-stock-qty-alerts')
           }
         }, {
-          path: 'report/inventory/transfer',
-          getComponent (nextState, cb) {
-            require.ensure([], (require) => {
-              registerModel(app, require('./models/report/inventory'))
-              cb(null, require('./routes/report/inventory/summary/'))
-            }, 'report-account-payment')
-          }
-        }, {
           path: 'report/accounts/payment',
           getComponent (nextState, cb) {
             require.ensure([], (require) => {
