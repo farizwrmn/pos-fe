6c4dee019ad492a119f17388b4a7c9c4aac6a274
veirryAu <veirryau@gmail.com>
2018-03-05 15:50:43 +0700

    fix: filterjs

--- src/routes/report/product/stock/Filter.js
+++ src/routes/report/product/stock/Filter.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment'
-import { Form, Row, Col, DatePicker, Input } from 'antd'
-import { FilterItem } from 'components'
+import { Form, Row, Col, Input } from 'antd'
 
 const Search = Input.Search
-const { RangePicker } = DatePicker
 
 const ColProps = {
   xs: 24,
@@ -22,42 +19,15 @@ const Filter = ({
     getFieldsValue
   }
 }) => {
-  const handleFields = (fields) => {
-    const { period } = fields
-    if (period.length) {
-      fields.period = [period[0].format('YYYY-MM-DD'), period[1].format('YYYY-MM-DD')]
-    }
-    return fields
-  }
-
   const handleSubmit = () => {
     let fields = getFieldsValue()
-    fields = handleFields(fields)
     onFilterChange(fields)
   }
-
-  const handleChange = (key, values) => {
-    let fields = getFieldsValue()
-    fields[key] = values
-    fields = handleFields(fields)
-    onFilterChange(fields)
-  }
-
-  let currentPeriod = []
-  currentPeriod[0] = moment().startOf('month')
-  currentPeriod[1] = moment()
 
   return (
     <Row gutter={24}>
       <Col {...ColProps} xl={{ span: 4 }} md={{ span: 8 }}>
         {getFieldDecorator('searchName')(<Search placeholder="Search Name" size="large" onSearch={handleSubmit} />)}
-      </Col>
-      <Col {...ColProps} xl={{ span: 6 }} md={{ span: 8 }} sm={{ span: 12 }}>
-        <FilterItem label="Period" >
-          {getFieldDecorator('period', { initialValue: currentPeriod })(
-            <RangePicker style={{ width: '100%' }} size="large" onChange={handleChange.bind(null, 'period')} />
-          )}
-        </FilterItem>
       </Col>
     </Row>
   )
