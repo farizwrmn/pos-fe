56667fecaf10f063c94423844c53583e40d75aa0
Ne0D3v <acc4develop@gmail.com>
2018-03-26 10:28:56 +0700

    update version to 2018.03.002

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ const APIURL = `http://${APIHOST}:${APIPORT}`
 
 module.exports = {
   name: 'DMI-PoS',
-  version: '2018.03.001-rev2',
+  version: '2018.03.002',
   prefix: 'dmiPos',
   footerText: 'Darkotech Mandiri Indonesia',
   footerSubText: 'Point of Sales © 2017-2018',
