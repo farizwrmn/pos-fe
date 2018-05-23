511a8835b16a07d08fe27f24ca320ef660ab71e9
Ne0D3v <acc4develop@gmail.com>
2018-05-23 12:02:38 +0700

    change config to demopos for testserver

--- src/utils/config.rest.js
+++ src/utils/config.rest.js
@@ -1,5 +1,5 @@
 const APIV1 = '/api/v1'
-const APIHOST = 'localhost'//'demopos.darkotech.id' // '192.168.80.64'
+const APIHOST = 'demopos.darkotech.id' // '192.168.80.64'
 const APIPORT = 5557
 const APIURL = `http://${APIHOST}:${APIPORT}`
 
