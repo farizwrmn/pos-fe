f6d5ebe0fba34516b396e95cf1e18e6c8f369edc
ploychompoo <jamesriady1998@yahoo.com>
2018-05-31 11:40:59 +0700

    fix: fixing bugs in menu

--- src/routes/setting/menu/Form.js
+++ src/routes/setting/menu/Form.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Form, Input, InputNumber, Row, Col, TreeSelect, Button, Modal } from 'antd'
+import List from './List'
 
 const FormItem = Form.Item
 const TreeNode = TreeSelect.TreeNode
@@ -18,11 +19,20 @@ const formItemLayout = {
   }
 }
 
+const column = {
+  sm: { span: 24 },
+  md: { span: 12 },
+  lg: { span: 12 },
+  xl: { span: 12 }
+}
+
 const FormInput = ({
+  ...otherProps,
   item,
   menuTree,
   modalType,
   onSubmit,
+  onEditItem,
   onCancel,
   button,
   form: {
@@ -33,6 +43,14 @@ const FormInput = ({
     resetFields
   }
 }) => {
+  let listProps = {
+    ...otherProps,
+    menuTree,
+    editItem () {
+      onEditItem()
+      resetFields()
+    }
+  }
   const tailFormItemLayout = {
     wrapperCol: {
       span: 24,
@@ -97,67 +115,74 @@ const FormInput = ({
   }
 
   return (
-    <Form layout="horizontal">
-      <Row>
-        <Col span={24}>
-          <FormItem label="Menu ID" hasFeedback {...formItemLayout}>
-            {getFieldDecorator('menuId', {
-              initialValue: item.menuId,
-              rules: [{ required: true }]
-            })(<InputNumber disabled={!!item.menuId} min={0} maxLength={10} style={{ width: '100%' }} />)}
-          </FormItem>
-          <FormItem label="Icon" hasFeedback {...formItemLayout}>
-            {getFieldDecorator('icon', {
-              initialValue: item.icon,
-              rules: [{ required: true }]
-            })(<Input maxLength={32} />)}
-          </FormItem>
-          <FormItem label="Name" hasFeedback {...formItemLayout}>
-            {getFieldDecorator('name', {
-              initialValue: item.name,
-              rules: [{ required: true }]
-            })(<Input maxLength={50} />)}
-          </FormItem>
-          <FormItem label="BPID" hasFeedback {...formItemLayout}>
-            {getFieldDecorator('bpid', {
-              initialValue: item.bpid
-            })(<TreeSelect
-              showSearch
-              allowClear
-              onSelect={value => onSelectBPID(value)}
-              placeholder="Select the BPID"
-              dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-              treeDefaultExpandAll
-            >
-              {getMenus(menuTree)}
-            </TreeSelect>)}
-          </FormItem>
-          <FormItem label="MPID" hasFeedback {...formItemLayout}>
-            {getFieldDecorator('mpid', {
-              initialValue: item.mpid
-            })(<TreeSelect
-              showSearch
-              allowClear
-              placeholder="Select the MPID"
-              dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-              treeDefaultExpandAll
-            >
-              <TreeNode value="-1" key="-1" title="None" />
-              {getMenus(menuTree)}
-            </TreeSelect>)}
-          </FormItem>
-          <FormItem label="Route" hasFeedback {...formItemLayout}>
-            {getFieldDecorator('route', {
-              initialValue: item.route
-            })(<Input />)}
-          </FormItem>
-          <FormItem {...tailFormItemLayout}>
-            {modalType === 'edit' && <Button type="danger" style={{ margin: '0 10px' }} onClick={handleCancel}>Cancel</Button>}
-            <Button type="primary" onClick={handleSubmit}>{button}</Button>
-          </FormItem>
-        </Col>
-      </Row>
-    </Form>
+    <Row>
+      <Col {...column}>
+        <Form layout="horizontal">
+          <Row>
+            <Col span={24}>
+              <FormItem label="Menu ID" hasFeedback {...formItemLayout}>
+                {getFieldDecorator('menuId', {
+                  initialValue: item.menuId,
+                  rules: [{ required: true }]
+                })(<InputNumber min={0} maxLength={10} style={{ width: '100%' }} />)}
+              </FormItem>
+              <FormItem label="Icon" hasFeedback {...formItemLayout}>
+                {getFieldDecorator('icon', {
+                  initialValue: item.icon,
+                  rules: [{ required: true }]
+                })(<Input maxLength={32} />)}
+              </FormItem>
+              <FormItem label="Name" hasFeedback {...formItemLayout}>
+                {getFieldDecorator('name', {
+                  initialValue: item.name,
+                  rules: [{ required: true }]
+                })(<Input maxLength={50} />)}
+              </FormItem>
+              <FormItem label="BPID" hasFeedback {...formItemLayout}>
+                {getFieldDecorator('bpid', {
+                  initialValue: item.bpid
+                })(<TreeSelect
+                  showSearch
+                  allowClear
+                  onSelect={value => onSelectBPID(value)}
+                  placeholder="Select the BPID"
+                  dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                  treeDefaultExpandAll
+                >
+                  {getMenus(menuTree)}
+                </TreeSelect>)}
+              </FormItem>
+              <FormItem label="MPID" hasFeedback {...formItemLayout}>
+                {getFieldDecorator('mpid', {
+                  initialValue: item.mpid
+                })(<TreeSelect
+                  showSearch
+                  allowClear
+                  placeholder="Select the MPID"
+                  dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                  treeDefaultExpandAll
+                >
+                  <TreeNode value="-1" key="-1" title="None" />
+                  {getMenus(menuTree)}
+                </TreeSelect>)}
+              </FormItem>
+              <FormItem label="Route" hasFeedback {...formItemLayout}>
+                {getFieldDecorator('route', {
+                  initialValue: item.route
+                })(<Input />)}
+              </FormItem>
+              <FormItem {...tailFormItemLayout}>
+                {modalType === 'edit' && <Button type="danger" style={{ margin: '0 10px' }} onClick={handleCancel}>Cancel</Button>}
+                <Button type="primary" onClick={handleSubmit}>{button}</Button>
+              </FormItem>
+            </Col>
+          </Row>
+        </Form>
+      </Col>
+      <Col {...column}>
+        <List {...listProps} />
+      </Col>
+    </Row>
   )
 }
 
