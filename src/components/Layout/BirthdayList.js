be5765bcd29c7c1e5e626d1d7c956a75e0781010
veirryAu <veirryau@gmail.com>
2018-06-08 13:42:49 +0700

    fix: typo using unexcaped html

--- src/components/Layout/BirthdayList.js
+++ src/components/Layout/BirthdayList.js
@@ -17,7 +17,7 @@ const BirthdayList = ({ ...modalProps, content }) => {
     <h2 style={{ fontWeight: 'bold' }}>
       {x.memberName}
     </h2>
-    <p>Today is {capitalize(x.memberName)}`s birthday. Let`s say happy birthday to {capitalize(x.memberName)}!</p>
+    <p>{`Today is ${capitalize(x.memberName)}'s birthday. Let's say happy birthday to ${capitalize(x.memberName)}!`}</p>
     <p>Email: {x.email ? capitalize(x.email) : '-'}</p>
     <p>Phone no: {x.mobileNumber ? capitalize(x.mobileNumber) : '-'}</p>
     <p><span style={{ float: 'left' }}>Birthdate: {x.birthDate ? moment(x.birthDate).format('DD-MMM-YYYY') : '-'}</span><Button style={{ float: 'right', border: '1px solid #d2d4d8' }}>Send Email</Button></p>
