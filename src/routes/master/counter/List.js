ba734c6cc09aa2b06f41c5ef747349fa909b9cbe
ploychompoo <jamesriady1998@yahoo.com>
2018-06-12 10:30:38 +0700

    add: new master counter menu

---
+++ src/routes/master/counter/List.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Table, Modal } from 'antd'
+import { DropOption } from 'components'
+import moment from 'moment'
+
+const confirm = Modal.confirm
+
+const List = ({ ...tableProps, editItem, deleteItem }) => {
+  const handleMenuClick = (record, e) => {
+    if (e.key === '1') {
+      editItem(record)
+    } else if (e.key === '2') {
+      confirm({
+        title: `Are you sure delete ${record.counterName} ?`,
+        onOk () {
+          deleteItem(record.id)
+        }
+      })
+    }
+  }
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'counterName',
+      key: 'counterName'
+    },
+    {
+      title: 'Description',
+      dataIndex: 'counterDesc',
+      key: 'counterDesc'
+    },
+    {
+      title: 'Created',
+      children: [
+        {
+          title: 'By',
+          dataIndex: 'createdBy',
+          key: 'createdBy'
+        },
+        {
+          title: 'Time',
+          dataIndex: 'createdAt',
+          key: 'createdAt',
+          render: text => (text ? moment(text).format('DD-MM-YYYY HH:mm:ss') : '')
+        }
+      ]
+    },
+    {
+      title: 'Updated',
+      children: [
+        {
+          title: 'By',
+          dataIndex: 'updatedBy',
+          key: 'updatedBy'
+        },
+        {
+          title: 'Time',
+          dataIndex: 'updatedAt',
+          key: 'updatedAt',
+          render: text => (text ? moment(text).format('DD-MM-YYYY HH:mm:ss') : '')
+        }
+      ]
+    },
+    {
+      title: 'Operation',
+      key: 'operation',
+      width: 100,
+      fixed: 'right',
+      render: (text, record) => {
+        return <DropOption onMenuClick={e => handleMenuClick(record, e)} menuOptions={[{ key: '1', name: 'Edit' }, { key: '2', name: 'Delete' }]} />
+      }
+    }
+  ]
+
+  return (
+    <div>
+      <Table {...tableProps}
+        bordered
+        columns={columns}
+        simple
+        scroll={{ x: 1000 }}
+        rowKey={record => record.id}
+      />
+    </div>
+  )
+}
+
+List.propTypes = {
+  editItem: PropTypes.func,
+  deleteItem: PropTypes.func
+}
+
+export default List
