290ff018be7c11a591a884189fa31ed6ec1f02de
ploychompoo <jamesriady1998@yahoo.com>
2018-07-23 08:56:18 +0700

    add: add cashier approve

---
+++ src/routes/monitor/cashier/approve/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { connect } from 'dva'
+import { Button, Modal } from 'antd'
+import Filter from './Filter'
+import Browse from './Browse'
+
+const Approve = ({ cashier, dispatch, loading }) => {
+  const { listRequestedCashRegister, currentItem } = cashier
+
+  const filterProps = {
+    findCashier (cashierId) {
+      dispatch({
+        type: 'cashier/getRequestedCashRegister',
+        payload: cashierId ? { cashierId } : {}
+      })
+    }
+  }
+
+  const browseProps = {
+    currentItem,
+    dataSource: listRequestedCashRegister,
+    loading: loading.effects['cashier/getRequestedCashRegister'],
+    checkRequestedCashRegister (checked, record) {
+      dispatch({
+        type: 'cashier/checkRequest',
+        payload: { checked, record }
+      })
+    }
+  }
+
+  const confirmRequest = (item) => {
+    Modal.confirm({
+      content: `Approve this request to open cashier period for ${item.cashierId}`,
+      onOk () {
+        console.log(item)
+        // dispatch({
+        //   type: 'cashier/approveRequestOpenCashRegister',
+        //   payload: { id: item.id, storeId: item.storeId }
+        // })
+      },
+      onCancel () { }
+    })
+  }
+
+  return (
+    <div className="content-inner" style={{ clear: 'both' }}>
+      <Filter {...filterProps} />
+      <div style={{ height: 10 }} />
+      <Browse {...browseProps} />
+      <Button disabled={!currentItem.id} onClick={() => confirmRequest(currentItem)} size="large" type="primary" style={{ position: 'absolute', right: 30, bottom: 30 }}>Confirm</Button>
+    </div>
+  )
+}
+
+export default connect(({ cashier, loading }) => ({ cashier, loading }))(Approve)
