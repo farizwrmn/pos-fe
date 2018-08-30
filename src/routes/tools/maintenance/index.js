58971251eef1846202c164705d32131f2a43dbb3
veirryAu <veirryau@gmail.com>
2018-08-30 14:19:27 +0700

    new maintenance tab with pathname as key

--- src/routes/tools/maintenance/index.js
+++ src/routes/tools/maintenance/index.js
@@ -1,153 +1,37 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Tabs } from 'antd'
 import { connect } from 'dva'
-import FormEdit from './FormEdit'
+import { routerRedux } from 'dva/router'
+import { Tabs } from 'antd'
+import PosHeader from '../posHeader'
+import Inventory from '../Inventory'
 
 const TabPane = Tabs.TabPane
 
-const Maintenance = ({ dispatch, maintenance, pos, payment }) => {
-  const { optionPos, listTrans, period, item, modalVisible, modalType } = maintenance
-  const { listMember, listMechanic, listUnit } = pos
-  const { usingWo, woNumber } = payment
-  const formWoProps = {
-    usingWo,
-    woNumber,
-    formItemLayout: {
-      labelCol: { span: 8 },
-      wrapperCol: { span: 10 },
-      style: {
-        marginBottom: '5px',
-        marginTop: '5px'
-      }
-    },
-    generateSequence () {
-      dispatch({
-        type: 'payment/sequenceQuery',
-        payload: {
-          seqCode: 'WO'
-        }
-      })
-    },
-    notUsingWo (check, value) {
-      dispatch({
-        type: 'payment/querySequenceSuccess',
-        payload: {
-          usingWo: check,
-          woNumber: value
-        }
-      })
-    }
-  }
-  const formEditProps = {
-    optionPos,
-    item,
-    modalType,
-    listUnit,
-    listMember,
-    listMechanic,
-    listTrans,
-    period,
-    modalVisible,
-    ...formWoProps,
-    onChangePeriod (start, end) {
-      dispatch({
-        type: 'maintenance/setAllNull'
-      })
-      dispatch({
-        type: 'maintenance/queryPos',
-        payload: {
-          startPeriod: start,
-          endPeriod: end
-        }
-      })
-    },
-    setFormItem (data) {
-      dispatch({
-        type: 'maintenance/setInitialValue',
-        payload: {
-          item: data
-        }
-      })
-    },
-    setItem (data) {
-      dispatch({
-        type: 'maintenance/setItem',
-        payload: {
-          item: data
-        }
-      })
-    },
-    onShowModal (e, memberId) {
-      if (e === 'member') {
-        dispatch({
-          type: 'pos/getMembers'
-        })
-        dispatch({
-          type: 'maintenance/showModal',
-          payload: {
-            modalType: e
-          }
-        })
-      } else if (e === 'mechanic') {
-        dispatch({
-          type: 'pos/getMechanics'
-        })
-        dispatch({
-          type: 'maintenance/showModal',
-          payload: {
-            modalType: e
-          }
-        })
-      } else if (e === 'unit') {
-        dispatch({
-          type: 'pos/getUnit',
-          payload: {
-            id: memberId
-          }
-        })
-        dispatch({
-          type: 'maintenance/showModal',
-          payload: {
-            modalType: e
-          }
-        })
-      }
-    },
-    onHideModal () {
-      dispatch({
-        type: 'maintenance/hideModal'
-      })
-    },
-    onOk (data) {
-      dispatch({
-        type: 'maintenance/update',
-        payload: {
-          id: data.transNoId,
-          data
-        }
-      })
-    }
-  }
+const Maintenance = ({ maintenance, dispatch }) => {
+  const { path } = maintenance
 
+  const changeTab = (path) => {
+    dispatch({
+      type: 'maintenance/updateState',
+      payload: {
+        path
+      }
+    })
+    dispatch(routerRedux.push(path))
+  }
   return (
     <div className="content-inner">
-      <Tabs>
-        <TabPane tab="POS Header" key="1"><FormEdit {...formEditProps} /></TabPane>
+      <Tabs type="card" activeKey={path} onChange={path => changeTab(path)}>
+        <TabPane tab="POS header" key="/tools/maintenance/posheader">
+          {path === '/tools/maintenance/posheader' && <PosHeader />}
+        </TabPane>
+        <TabPane tab="Recalculate Inventory" key="/tools/maintenance/inventory">
+          {path === '/tools/maintenance/inventory' && <Inventory />}
+        </TabPane>
       </Tabs>
     </div>
   )
 }
-Maintenance.propTypes = {
-  maintenance: PropTypes.object,
-  pos: PropTypes.object,
-  payment: PropTypes.object,
-  unit: PropTypes.object,
-  app: PropTypes.object,
-  location: PropTypes.object,
-  dispatch: PropTypes.func,
-  loading: PropTypes.object
-}
 
+export default connect(({ maintenance }) => ({ maintenance }))(Maintenance)
 
-export default connect(({ maintenance, pos, payment, unit, loading, app }) => ({ maintenance, pos, payment, unit, loading, app }))(Maintenance)
