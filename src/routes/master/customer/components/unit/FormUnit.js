830b044c8343a135fab253fc97abc72db3bdc127
veirryAu <veirryau@gmail.com>
2018-08-30 14:18:06 +0700

    fix: formUnit InvalidDate on expired field

--- src/routes/master/customer/components/unit/FormUnit.js
+++ src/routes/master/customer/components/unit/FormUnit.js
@@ -238,7 +238,7 @@ const FormUnit = ({
       </FormItem>
       <FormItem label="Expiration Date" hasFeedback {...formItemLayout}>
         {getFieldDecorator('expired', {
-          initialValue: item.expired ? moment(item.expired, 'YYYY-MM-DD') : null
+          initialValue: item.expired ? moment.utc(item.expired, 'YYYY-MM-DD').format('MMM-YYYY') : null
         })(<MonthPicker format="MMM-YYYY" placeholder="Select period" />)}
       </FormItem>
       {(modalType === 'edit' || modalType === 'add') &&
