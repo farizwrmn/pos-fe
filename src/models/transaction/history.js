57755b5944ed49ed1e4aac6779c4c5d977b939e8
veirryAu <veirryau@gmail.com>
2018-08-30 14:20:50 +0700

    remove routerRedux push in model subscriptions

--- src/models/transaction/history.js
+++ src/models/transaction/history.js
@@ -1,5 +1,4 @@
 import modelExtend from 'dva-model-extend'
-import { routerRedux } from 'dva/router'
 import { pageModel } from '../common'
 
 export default modelExtend(pageModel, {
@@ -11,9 +10,6 @@ export default modelExtend(pageModel, {
     setup ({ dispatch, history }) {
       history.listen((location) => {
         if (location.pathname === '/transaction/history') {
-          dispatch(routerRedux.push({
-            pathname: '/transaction/pos/history'
-          }))
           dispatch({ type: 'updateState', payload: { path: '/transaction/pos/history' } })
         } else {
           dispatch({ type: 'updateState', payload: { path: location.pathname } })
