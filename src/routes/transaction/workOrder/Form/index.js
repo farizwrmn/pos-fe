a694b2c336abe381c8cd317a0d11860dca0e5274
ploychompoo <jamesriady1998@yahoo.com>
2018-08-31 16:53:23 +0700

    add: transaction work order

--- src/routes/transaction/workOrder/Form/index.js
+++ src/routes/transaction/workOrder/Form/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Steps } from 'antd'
+import { WO } from './components'
 
 const Step = Steps.Step
 
 const Form = ({ currentStep }) => {
   const steps = [
-    { key: 0, title: 'Workorder', content: <div /> },
+    { key: 0, title: 'Workorder', content: <WO /> },
     { key: 1, title: 'Custom Field', content: <div /> },
     { key: 2, title: 'View', content: <div /> }
   ]
@@ -15,7 +16,7 @@ const Form = ({ currentStep }) => {
       <Steps current={currentStep}>
         {steps.map(x => <Step key={x.key} title={x.title} />)}
       </Steps>
-      <div className="steps-content">{steps[currentStep].content}</div>
+      <div className="steps-content" style={{ marginTop: 20 }}>{steps[currentStep].content}</div>
     </div>
   )
 }
