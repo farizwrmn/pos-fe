9200d9a82f96892fed53636ab5dacd6f4e7a63cc
veirryAu <veirryau@gmail.com>
2018-10-02 10:28:53 +0700

    add jest to repository

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,14 @@ module.exports = (webpackConfig) => {
     enums: `${__dirname}/src/utils/enums`,
     themes: `${__dirname}/src/themes`
   }
+  webpackConfig.resolve.modules = ['node_modules']
+
+  webpackConfig.loaders = [
+    { exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/ },
+    { loader: 'style-loader!css-loader', test: /\.css$/ },
+    { loader: 'url-loader', test: /\.gif$/ },
+    { loader: 'file-loader', test: /\.(ttf|eot|svg)$/ }
+  ]
 
   return webpackConfig
 }
