a5afcecc85916a3e7b9636def0187d0d3e682f7a
VeirryAu <veirryau@gmail.com>
2020-02-14 18:50:30 +0700

    member dan product jadi 2 input

---
+++ src/routes/transaction/pos/BarcodeInput.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Input } from 'antd'
+
+class BarcodeInput extends Component {
+  state = {
+    product: '',
+    member: ''
+  }
+
+  render () {
+    const { onEnter } = this.props
+    return (
+      <div>
+        <Input
+          size="medium"
+          style={{ fontSize: 24, marginTop: 5, marginBottom: 8 }}
+          value={this.state.member}
+          onChange={(event) => {
+            this.setState({ member: event.target.value })
+          }}
+          placeholder="Member"
+          onPressEnter={(event) => {
+            onEnter(event, 'member')
+            this.setState({ member: '' })
+          }}
+        />
+        <Input
+          id="input-product"
+          size="medium"
+          autoFocus
+          value={this.state.product}
+          onChange={(event) => {
+            this.setState({ product: event.target.value })
+          }}
+          style={{ fontSize: 24, marginBottom: 8 }}
+          placeholder="Product"
+          onPressEnter={(event) => {
+            onEnter(event, 'barcode')
+            this.setState({ product: '' })
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+BarcodeInput.propTypes = {
+  onEnter: PropTypes.func.isRequired
+}
+
+export default BarcodeInput
