07356afe0bd5bbbfa4dc12c567ee6f9f842f5abd
VeirryAu <veirryau@gmail.com>
2020-04-02 21:36:46 +0700

    accountCode validation

--- src/routes/master/cashEntryType/Form.js
+++ src/routes/master/cashEntryType/Form.js
@@ -91,7 +91,7 @@ const FormCounter = ({
               rules: [
                 {
                   required: true,
-                  pattern: /^[a-z0-9-/]{3,9}$/i
+                  pattern: /^[a-z0-9-/]{1,9}$/i
                 }
               ]
             })(<Input maxLength={3} autoFocus />)}
