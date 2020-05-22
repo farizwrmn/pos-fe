72613c3cb2865380e2467f5fce2cf5633577f727
VeirryAu <veirryau@gmail.com>
2020-05-22 10:48:43 +0700

    fix wrong user consignment

--- src/services/master/consignment.js
+++ src/services/master/consignment.js
@@ -2,7 +2,7 @@ import { request, crypt, lstorage } from '../../utils'
 
 export async function query (params) {
   const apiHeaderToken = crypt.apiheader()
-  params.outlet_id = lstorage.getCurrentUserStore()
+  params.outlet_id = lstorage.getCurrentUserConsignment()
   if (params && params.outlet_id) {
     return request({
       url: '/consignment-api/stock',
