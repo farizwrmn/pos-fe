600db4a240f77be625ec74eca8b14323ae2b792e
VeirryAu <veirryau@gmail.com>
2020-05-29 14:33:13 +0700

    get list balance

--- src/routes/balance/history/List.js
+++ src/routes/balance/history/List.js
@@ -1,22 +1,56 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 import { Calendar } from 'antd'
+import moment from 'moment'
+import styles from './List.less'
 
-const List = () => {
+const List = ({
+  listBalance,
+  dispatch
+}) => {
+  function getListData (value) {
+    const filteredData = listBalance.filter(filtered => moment(filtered.open).format('YYYY-MM-DD') === value.format('YYYY-MM-DD'))
+    let listData = filteredData
+    return listData || []
+  }
+
+  function dateCellRender (value) {
+    const listData = getListData(value)
+    return (
+      <ul className="events">
+        {
+          listData.map((item, index) => (
+            <li key={index}>
+              <span className={item.closed ? styles.eventClose : styles.eventOpen}>{`● ${item.shift.shiftName} - ${item.closed ? 'closed' : 'open'}`}</span>
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
   const calendarProps = {
     mode: 'month',
-    fullscreen: true
-    // dateCellRender,
+    fullscreen: true,
+    dateCellRender,
     // onSelect (value) {
     //   showBirthDayListModal(value)
     // },
-    // onPanelChange (value, mode) {
-    //   const month = moment(value).format('MM')
-    //   changeCalendarMode(month, mode)
-    // },
-    // disabledDate (current) {
-    //   return current <= moment().startOf('day')
-    // }
+    onPanelChange (value) {
+      const month = moment(value).format('MM')
+      const year = moment(value).format('YYYY')
+      dispatch({
+        type: 'balance/query',
+        payload: {
+          relationship: 1,
+          history: 1,
+          month,
+          year
+        }
+      })
+    },
+    disabledDate (current) {
+      return current >= moment().startOf('day')
+    }
   }
 
   return (
--- src/routes/balance/history/index.js
+++ src/routes/balance/history/index.js
@@ -1,27 +1,40 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import { connect } from 'dva'
 import List from './List'
 
-const Container = () => {
+const Container = ({
+  dispatch,
+  balance
+}) => {
+  const { listBalance } = balance
+
+  const listProps = {
+    dispatch,
+    listBalance
+  }
+
   return (
     <div className="content-inner">
-      <List />
+      <List {...listProps} />
     </div>
   )
 }
 
 Container.propTypes = {
+  balance: PropTypes.object,
   // loading: PropTypes.object,
-  // dispatch: PropTypes.func
+  dispatch: PropTypes.func
 }
 
 export default connect(
   ({
+    balance,
     loading,
     app
   }) =>
     ({
+      balance,
       loading,
       app
     })
