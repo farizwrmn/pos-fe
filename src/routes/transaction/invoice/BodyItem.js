7b595bb3e168e8b66c63a83401eeaf203435d912
VeirryAu <veirryau@gmail.com>
2020-07-17 16:19:35 +0700

    product name

--- src/routes/transaction/invoice/BodyItem.js
+++ src/routes/transaction/invoice/BodyItem.js
@@ -7,7 +7,7 @@ const BodyItem = ({ item }) => {
   return (
     <div className={styles.item}>
       <Row>
-        <Col span={12} className={styles.left}>{item.name} - {item.code}</Col>
+        <Col span={24} className={styles.left}>{item.name} - {item.code}</Col>
       </Row>
       <Row>
         <Col span={12} className={styles.left}>{numberFormatter(item.qty)} x @{numberFormatter(item.price)}</Col>
