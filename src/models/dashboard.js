23803b8d358cf32e993ac905c3fe0c3e04d39d42
VeirryAu <veirryau@gmail.com>
2020-09-21 00:10:33 +0700

    add copy total sales and stock

--- src/models/dashboard.js
+++ src/models/dashboard.js
@@ -1,8 +1,29 @@
+import { message } from 'antd'
 import moment from 'moment'
+import { configMain, lstorage } from 'utils'
+import {
+  queryByDate
+} from 'services/report/pos'
 import { getDashboards } from '../services/dashboard'
 import {
   queryFifoCategory
 } from '../services/report/fifo'
+import { numberFormatter } from '../utils/numberFormat'
+
+const { prefix } = configMain
+
+const onCopy = (endpoint) => {
+  let textarea = document.createElement('textarea')
+  textarea.id = 'temp_element'
+  textarea.style.height = 0
+  document.body.appendChild(textarea)
+  textarea.value = endpoint
+  let selector = document.querySelector('#temp_element')
+  selector.select()
+  document.execCommand('copy')
+  document.body.removeChild(textarea)
+  message.success('Success to key to clipboard')
+}
 
 const construct = (dataSales, startDate, toDate) => {
   const diffDay = moment(startDate, 'YYYY-MM-DD').diff(moment(toDate, 'YYYY-MM-DD'), 'days') + 1
@@ -78,16 +99,61 @@ export default {
       yield put({ type: 'querySuccess', payload: { data: formatData, ...payload } })
     },
     * querySalesCategory (payload, { call, put }) {
-      const response = yield call(queryFifoCategory, {
-        from: '',
-        to: '',
-        category: '183'
-      })
+      let copiedText = ''
+
+      const storeInfo = localStorage.getItem(`${prefix}store`) ? JSON.parse(localStorage.getItem(`${prefix}store`)) : {}
+
+      const listUserStores = lstorage.getListUserStores()
       const responseStock = yield call(queryFifoCategory, {
-        from: '',
-        to: '',
-        category: '183'
+        from: storeInfo.startPeriod,
+        to: moment().format('YYYY-MM-DD'),
+        category: '117,118,121,122,125'
       })
+      const responseSales = yield call(queryByDate, {
+        from: moment().subtract(2, 'day').format('YYYY-MM-DD'),
+        to: moment().add(2, 'day').format('YYYY-MM-DD')
+      })
+      if (responseSales && responseSales.success) {
+        for (let key in listUserStores) {
+          const dataItem = listUserStores[key]
+          if (dataItem) {
+            copiedText += `${dataItem.label}\n`
+          }
+          const record = responseSales
+            .data
+            .filter(filtered => filtered.storeId === dataItem.value)
+          copiedText += record
+            .map(item => (`${item.categoryName}\t${numberFormatter(item.netto)}`))
+            .toString()
+            .replace(/,/g, '\n')
+          copiedText += `\nTotal\t${numberFormatter(record.reduce((prev, next) => prev + parseFloat(next.netto), 0))}`
+          // .toString()
+          // .replace(/,/g, '\n')
+          copiedText += '\n\n'
+        }
+      }
+      if (responseStock && responseStock.success) {
+        for (let key in listUserStores) {
+          const dataItem = listUserStores[key]
+          if (dataItem) {
+            copiedText += `UPDATE STOCK SEPEDA ${moment().format('DD MMMM YYYY')}\n${dataItem.label}\n\n`
+          }
+          copiedText += responseStock
+            .data
+            .filter(filtered => filtered.count)
+            .filter(filtered => filtered.storeId === dataItem.value)
+            .map(item => (`${item.productName}(${item.productCode})\t${item.count}x${numberFormatter(item.sellPrice)}`))
+            .toString()
+            .replace(/,/g, '\n')
+          copiedText += '\n\n'
+        }
+      }
+      onCopy(copiedText)
+      // const responseSales = yield call(queryFifoCategory, {
+      //   from: '',
+      //   to: '',
+      //   category: '183'
+      // })
       yield put({
         type: 'querySuccess',
         payload: {
