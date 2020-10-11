0cd2bd716503b3af4393f5c4b8b69e01ee8b5393
VeirryAu <veirryau@gmail.com>
2020-10-11 21:58:55 +0700

    fix checking for posTotal

--- src/utils/total.js
+++ src/utils/total.js
@@ -20,7 +20,7 @@ const posTotal = (data) => {
     throw new Error('disc3 cannot be null')
   }
 
-  let H1 = ((parseFloat(data.sellingPrice || data.sellPrice) * parseFloat(data.qty))) * (1 - (data.disc1 / 100))
+  let H1 = ((parseFloat(data.sellingPrice == null ? data.sellPrice : data.sellingPrice) * parseFloat(data.qty))) * (1 - (data.disc1 / 100))
   let H2 = H1 * (1 - (data.disc2 / 100))
   let H3 = H2 * (1 - (data.disc3 / 100))
   let TOTAL = H3 - data.discount
