cd576ee09dea9554c8a659d4de51c8c6c6faab96
VeirryAu <veirryau@gmail.com>
2021-02-02 10:09:31 +0700

    remove productId

--- src/routes/master/product/stock/List.js
+++ src/routes/master/product/stock/List.js
@@ -97,7 +97,6 @@ const List = ({ ...tableProps,
       render: (text, record) => {
         return (
           <div>
-            <div><strong>{record.id}</strong></div>
             <div><strong>{record.productCode}</strong></div>
             <div>{record.productName}</div>
           </div>
