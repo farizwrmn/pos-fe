2a0812b68196b6b6494178229d4acb428fcfd3e5
VeirryAu <veirryau@gmail.com>
2021-06-13 21:32:39 +0700

    fix env

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,8 @@
 const fs = require('fs')
 
-fs.writeFileSync('./.env', `API_ENDPOINT=${process.env.API_ENDPOINT || 'localhost'}`)
+fs.writeFileSync('./.env', `
+API_ENDPOINT=${process.env.API_ENDPOINT || 'localhost'}
+CLOUDANT_NAME=${process.env.CLOUDANT_NAME || 'localhost'}
+APP_NAME=${process.env.APP_NAME || 'k3mart'}
+CLOUDANT_URL=${process.env.CLOUDANT_URL || 'localhost'}
+`)
