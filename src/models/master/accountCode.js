9ea3da89853a60a943d77a91e5f6dce093718d8b
VeirryAu <veirryau@gmail.com>
2021-10-11 14:17:16 +0700

    fix accountCode and disable accounting CoA

--- src/models/master/accountCode.js
+++ src/models/master/accountCode.js
@@ -58,23 +58,6 @@ export default modelExtend(pageModel, {
             }
           })
         }
-        if (pathname === '/accounts/payable-form') {
-          dispatch({
-            type: 'queryExpense',
-            payload: {
-              type: 'all',
-              order: 'accountCode'
-            }
-          })
-          dispatch({
-            type: 'query',
-            payload: {
-              accountType: 'BANK',
-              type: 'all',
-              order: 'accountCode'
-            }
-          })
-        }
         if (pathname === '/cash-entry'
           || pathname === '/transfer-entry'
           || pathname === '/bank-recon'
@@ -114,6 +97,7 @@ export default modelExtend(pageModel, {
         }
         if (
           pathname === '/master/account'
+          || pathname === '/accounts/payable-form'
           || pathname === '/journal-entry') {
           dispatch({
             type: 'updateState',
