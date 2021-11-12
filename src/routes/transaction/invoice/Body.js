b4c21279f4972f97bf4ab5ddf3ef8de3817b21be
VeirryAu <veirryau@gmail.com>
2021-11-12 15:42:59 +0700

    stop group

--- src/routes/transaction/invoice/Body.js
+++ src/routes/transaction/invoice/Body.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import BodyItem from './BodyItem'
 import Group from './Group'
-import GroupShow from './GroupShow'
 import styles from './index.less'
 
 const Body = ({
-  user,
-  standardInvoice,
   dataPos = [],
   dataService = [],
   dataGroup = [],
   dataConsignment = []
 }) => {
-  let role = false
-  if (user && user.permissions && user.permissions.role) {
-    role = user.permissions.role === 'SPR' || user.permissions.role === 'OWN' || user.permissions.role === 'ADM'
-  }
   return (
     <div>
       <div className={styles.borderedSection}>
@@ -34,20 +27,11 @@ const Body = ({
             <BodyItem key={index} item={item} />
           )
         })}
-        {standardInvoice && dataGroup && dataGroup.map((item, index) => {
+        {dataGroup && dataGroup.map((item, index) => {
           return (
             <Group key={index} item={item} />
           )
         })}
-        {!standardInvoice && role && dataGroup ? dataGroup.map((item, index) => {
-          return (
-            <GroupShow key={index} item={item} />
-          )
-        }) : (!standardInvoice && role && dataGroup ? dataGroup.map((item, index) => {
-          return (
-            <Group key={index} item={item} />
-          )
-        }) : null)}
       </div>
     </div>
   )
