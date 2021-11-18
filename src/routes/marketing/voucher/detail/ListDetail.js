d929ec49b5656e368493bd18c4361b67b0f81d4a
Veirry Augusman <aiinterface@gmail.com>
2021-11-18 17:09:39 +0700

    add operation to detail voucher

--- src/routes/marketing/voucher/detail/ListDetail.js
+++ src/routes/marketing/voucher/detail/ListDetail.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Table } from 'antd'
+import { DropOption } from 'components'
 
 const List = ({ ...tableProps, editList }) => {
   const handleMenuClick = (record) => {
@@ -43,6 +44,15 @@ const List = ({ ...tableProps, editList }) => {
       dataIndex: 'usageDescription',
       key: 'usageDescription',
       width: 300
+    },
+    {
+      title: 'Operation',
+      key: 'operation',
+      width: 100,
+      fixed: 'right',
+      render: (text, record) => {
+        return <DropOption onMenuClick={e => handleMenuClick(record, e)} menuOptions={[{ key: '1', name: 'Payment' }]} />
+      }
     }
   ]
 
