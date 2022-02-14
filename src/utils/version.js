7ec8833a1e03e5387f09bbae07efeee8987ce8c9
Veirry Augusman <aiinterface@gmail.com>
2022-02-14 14:01:46 +0700

    version finish

--- src/utils/version.js
+++ src/utils/version.js
@@ -1,31 +1,32 @@
 export const listVersion = [
 
   {
-    version: '2022.03.001',
+    version: '2022.03.003',
     info: `
     Update Info:
     1. Grabfood Integration
     2. Cancel POS akan auto Retur Barang Consignment
     `,
-    date: '2022-03-15'
+    date: '2022-03-14'
   },
   {
-    version: '2022.02.003',
+    version: '2022.03.002',
     info: `
     Update Info:
     1. VB App For Order Integration with Transfer Out
     2. Penagihan Voucher Wajib Member
     `,
-    date: '2022-02-27'
+    date: '2022-03-04'
   },
   {
-    version: '2022.02.002',
+    version: '2022.03.001',
     info: `
     Update Info:
     1. Delivery Order App Integration
     2. Register Member Website Ada Approval dari Kasir
+    3. Country Product
     `,
-    date: '2022-02-23'
+    date: '2022-02-24'
   },
   {
     version: '2022.02.001',
@@ -35,8 +36,7 @@ export const listVersion = [
     2. Show Dimension in Stock Report and Master Product
     3. Inventory Age in FIFO Inventory Value
     4. Price Tag Regular
-    5. Country Product
     `,
-    date: '2022-02-16'
+    date: '2022-02-17'
   }
 ]
