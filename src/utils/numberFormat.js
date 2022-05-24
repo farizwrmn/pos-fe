2df14ded6e472f2a296dca595a81449d032b6e90
VeirryAu <veirryau@gmail.com>
2022-05-24 11:35:40 +0700

    demand and excel

--- src/utils/numberFormat.js
+++ src/utils/numberFormat.js
@@ -1,7 +1,7 @@
 const formatNumberInExcel = (value, decimal) => {
   if (value !== 0 && typeof value === 'number') {
     let defaultFormat = '#,##0'
-    if (decimal > 0) {
+    if (decimal > 0 && value % 1 !== 0) {
       defaultFormat += '.'
       for (let i = 0; i < decimal; i += 1) {
         defaultFormat += '0'
