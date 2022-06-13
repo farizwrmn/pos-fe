013321fa9b924597d3d8d36d1dcd9d8652c959ff
VeirryAu <veirryau@gmail.com>
2022-06-13 10:35:04 +0700

    purchase order lov finishing

--- src/routes/transaction/purchase/ListInvoice.js
+++ src/routes/transaction/purchase/ListInvoice.js
@@ -1,48 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Table, Button, Input, Form, Row, Col } from 'antd'
+import { Table } from 'antd'
 import { connect } from 'dva'
 import styles from 'themes/index.less'
 import { numberFormatter } from 'utils/string'
 
-const FormItem = Form.Item
-
 const ListInvoice = ({ onInvoiceHeader, listPurchaseOrder, onChooseInvoice, purchase, dispatch, ...tableProps }) => {
-  const { searchText, tmpInvoiceList } = purchase
-
   const handleMenuClick = (record) => {
     onChooseInvoice(record)
-  }
-
-  const handleChange = (e) => {
-    const { value } = e.target
-
-    dispatch({
-      type: 'purchase/updateState',
-      payload: {
-        searchText: value
-      }
-    })
-  }
-
-  const handleSearch = () => {
-    dispatch({
-      type: 'purchase/onInvoiceSearch',
-      payload: {
-        searchText,
-        tmpInvoiceList
-      }
-    })
-  }
-
-  const handleReset = () => {
-    dispatch({
-      type: 'purchase/onInvoiceReset',
-      payload: {
-        searchText: '',
-        tmpInvoiceList
-      }
-    })
   }
 
   const columns = [
@@ -84,28 +49,6 @@ const ListInvoice = ({ onInvoiceHeader, listPurchaseOrder, onChooseInvoice, purc
 
   return (
     <div>
-      <Form layout="inline">
-        <Row>
-          <Col lg={16} md={14}>
-            <FormItem>
-              <Input placeholder="Search Invoice Number"
-                value={searchText}
-                size="small"
-                onChange={_e => handleChange(_e)}
-                onPressEnter={handleSearch}
-                style={{ marginBottom: 16 }}
-              />
-            </FormItem>
-            <FormItem>
-              <Button size="small" type="primary" onClick={handleSearch}>Search</Button>
-            </FormItem>
-            <FormItem>
-              <Button size="small" type="primary" onClick={handleReset}>Reset</Button>
-            </FormItem>
-          </Col>
-        </Row>
-      </Form>
-
       <Table
         {...tableProps}
         title={() => `Total: ${numberFormatter(listPurchaseOrder ? listPurchaseOrder.reduce((prev, next) => prev + (next.total), 0) : 0)}`}
