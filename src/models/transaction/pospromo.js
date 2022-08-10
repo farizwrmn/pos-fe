8ca19fced3447f21a9841d9f96ca50410dc35c4f
VeirryAu <veirryau@gmail.com>
2022-08-10 11:46:59 +0700

    remark bundle discount

--- src/models/transaction/pospromo.js
+++ src/models/transaction/pospromo.js
@@ -2,14 +2,15 @@ import modelExtend from 'dva-model-extend'
 import { message, Modal } from 'antd'
 import { posTotal, numberFormat } from 'utils'
 import {
-  getCashierTrans, getServiceTrans, getConsignment, getBundleTrans, getGrabmartOrder,
+  getCashierTrans, getServiceTrans, getConsignment, getBundleTrans,
+  // getGrabmartOrder,
   setCashierTrans, setServiceTrans, setBundleTrans
 } from 'utils/lstorage'
 import reduce from 'lodash/reduce'
 import { query } from '../../services/marketing/bundling'
 import { query as queryReward } from '../../services/marketing/bundlingReward'
 import { pageModel } from './../common'
-import { getDiscountByBundleCode } from './utils'
+// import { getDiscountByBundleCode } from './utils'
 
 const numberFormatter = numberFormat.numberFormatter
 
@@ -424,14 +425,14 @@ export default modelExtend(pageModel, {
             arrayProd.push(data)
           }
         }
-        const currentGrabOrder = getGrabmartOrder()
-        arrayProd = arrayProd.map((item) => {
-          if (item.bundleCode) {
-            item.discount = getDiscountByBundleCode(currentGrabOrder, item.bundleCode, arrayProd)
-            item.total = posTotal(item)
-          }
-          return item
-        })
+        // const currentGrabOrder = getGrabmartOrder()
+        // arrayProd = arrayProd.map((item) => {
+        //   if (item.bundleCode) {
+        //     item.discount = getDiscountByBundleCode(currentGrabOrder, item.bundleCode, arrayProd)
+        //     item.total = posTotal(item)
+        //   }
+        //   return item
+        // })
         console.log('setProductPos', arrayProd)
         setCashierTrans(JSON.stringify(arrayProd))
       }
