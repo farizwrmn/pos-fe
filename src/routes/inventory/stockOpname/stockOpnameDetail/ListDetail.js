448acab59245e7462b5c2931be4e1d7345bbaf70
VeirryAu <veirryau@gmail.com>
2022-09-13 17:00:04 +0700

    stockOpname

---
+++ src/routes/inventory/stockOpname/stockOpnameDetail/ListDetail.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Table } from 'antd'
+import { numberFormat } from 'utils'
+import styles from '../../../../themes/index.less'
+
+const formatNumberIndonesia = numberFormat.formatNumberIndonesia
+
+const List = ({ editList, ...tableProps }) => {
+  const handleMenuClick = (record) => {
+    editList(record)
+  }
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      width: 40
+    },
+    {
+      title: 'Account Code',
+      dataIndex: 'accountCode.accountCode',
+      key: 'accountCode.accountCode',
+      width: 100
+    },
+    {
+      title: 'Account Name',
+      dataIndex: 'accountCode.accountName',
+      key: 'accountCode.accountName',
+      width: 200
+    },
+    {
+      title: 'Debit',
+      dataIndex: 'amountIn',
+      key: 'amountIn',
+      width: 120,
+      className: styles.alignRight,
+      render: text => formatNumberIndonesia(text || 0)
+    },
+    {
+      title: 'Credit',
+      dataIndex: 'amountOut',
+      key: 'amountOut',
+      width: 120,
+      className: styles.alignRight,
+      render: text => formatNumberIndonesia(text || 0)
+    }
+  ]
+
+  return (
+    <div>
+      <Table {...tableProps}
+        bordered={false}
+        scroll={{ x: 500, y: 270 }}
+        columns={columns}
+        simple
+        rowKey={record => record.no}
+        onRowClick={record => handleMenuClick(record)}
+      />
+    </div>
+  )
+}
+
+List.propTypes = {
+  editList: PropTypes.func
+}
+
+export default List
--- src/routes/inventory/stockOpname/stockOpnameDetail/index.js
+++ src/routes/inventory/stockOpname/stockOpnameDetail/index.js
@@ -2,151 +2,66 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'dva'
 import { routerRedux } from 'dva/router'
-import { Button, Tabs } from 'antd'
-import Form from './Form'
-import List from './List'
-import Filter from './Filter'
+import {
+  Row,
+  Col,
+  // Tag,
+  Button
+} from 'antd'
+import TransDetail from './TransDetail'
+import styles from './index.less'
 
-const TabPane = Tabs.TabPane
 
-const Counter = ({ stockOpname, loading, dispatch, location, app }) => {
-  const { list, pagination, modalType, currentItem, activeKey } = stockOpname
-  const { user, storeInfo } = app
-  const filterProps = {
-    onFilterChange (value) {
-      dispatch({
-        type: 'stockOpname/query',
-        payload: {
-          ...value
-        }
-      })
-    }
-  }
-
-  const listProps = {
-    dataSource: list,
-    user,
-    storeInfo,
-    pagination,
-    loading: loading.effects['stockOpname/query'],
-    location,
-    onChange (page) {
-      const { query, pathname } = location
-      dispatch(routerRedux.push({
-        pathname,
-        query: {
-          ...query,
-          page: page.current,
-          pageSize: page.pageSize
-        }
-      }))
-    },
-    editItem (item) {
-      const { pathname } = location
-      dispatch(routerRedux.push({
-        pathname,
-        query: {
-          activeKey: 0
-        }
-      }))
-      dispatch({
-        type: 'stockOpname/editItem',
-        payload: { item }
-      })
-    },
-    deleteItem (id) {
-      dispatch({
-        type: 'stockOpname/delete',
-        payload: id
-      })
-    }
-  }
-
-  const changeTab = (key) => {
-    dispatch({
-      type: 'stockOpname/changeTab',
-      payload: { key }
-    })
-    const { query, pathname } = location
-    dispatch(routerRedux.push({
-      pathname,
-      query: {
-        ...query,
-        activeKey: key
+const Detail = ({ stockOpname, dispatch }) => {
+  const { listDetail, data } = stockOpname
+  const content = []
+  for (let key in data) {
+    if ({}.hasOwnProperty.call(data, key)) {
+      if (key !== 'policeNoId' && key !== 'storeId' && key !== 'id' && key !== 'memberId') {
+        content.push(
+          <div key={key} className={styles.item}>
+            <div>{key}</div>
+            <div>{String(data[key])}</div>
+          </div>
+        )
       }
-    }))
-    dispatch({ type: 'stockOpname/updateState', payload: { list: [] } })
-  }
-
-  const clickBrowse = () => {
-    dispatch({
-      type: 'stockOpname/updateState',
-      payload: {
-        activeKey: '1'
-      }
-    })
-  }
-
-  const formProps = {
-    modalType,
-    item: currentItem,
-    button: `${modalType === 'add' ? 'Add' : 'Update'}`,
-    onSubmit (data, reset) {
-      dispatch({
-        type: `stockOpname/${modalType}`,
-        payload: {
-          data,
-          reset
-        }
-      })
-    },
-    onCancel () {
-      const { pathname } = location
-      dispatch(routerRedux.push({
-        pathname,
-        query: {
-          activeKey: '1'
-        }
-      }))
-      dispatch({
-        type: 'stockOpname/updateState',
-        payload: {
-          currentItem: {}
-        }
-      })
     }
   }
 
-  let moreButtonTab
-  if (activeKey === '0') {
-    moreButtonTab = <Button onClick={() => clickBrowse()}>Browse</Button>
+  const BackToList = () => {
+    dispatch(routerRedux.push('/stock-opname?activeKey=0'))
   }
 
-  return (
-    <div className="content-inner">
-      <Tabs activeKey={activeKey} onChange={key => changeTab(key)} tabBarExtraContent={moreButtonTab} type="card">
-        <TabPane tab="Form" key="0" >
-          {activeKey === '0' && <Form {...formProps} />}
-        </TabPane>
-        <TabPane tab="Browse" key="1" >
-          {activeKey === '1' &&
-            <div>
-              <Filter {...filterProps} />
-              <List {...listProps} />
-            </div>
-          }
-        </TabPane>
-      </Tabs>
-    </div>
-  )
+  const formDetailProps = {
+    dataSource: listDetail
+  }
+
+  return (<div className="wrapper">
+    <Row>
+      <Col lg={6}>
+        <div className="content-inner-zero-min-height">
+          <Button type="primary" icon="rollback" onClick={() => BackToList()}>Back</Button>
+          <h1>Invoice Info</h1>
+          <div className={styles.content}>
+            {content}
+          </div>
+        </div>
+      </Col>
+      <Col lg={18}>
+        <div className="content-inner-zero-min-height">
+          <h1>Items</h1>
+          <Row style={{ padding: '10px', margin: '4px' }}>
+            <TransDetail {...formDetailProps} />
+          </Row>
+        </div>
+      </Col>
+    </Row>
+  </div>)
 }
 
-Counter.propTypes = {
-  stockOpname: PropTypes.object,
-  loading: PropTypes.object,
-  location: PropTypes.object,
+Detail.propTypes = {
   app: PropTypes.object,
-  dispatch: PropTypes.func
+  stockOpname: PropTypes.object
 }
 
-export default connect(({ stockOpname, loading, app }) => ({ stockOpname, loading, app }))(Counter)
+export default connect(({ app, stockOpname }) => ({ app, stockOpname }))(Detail)
