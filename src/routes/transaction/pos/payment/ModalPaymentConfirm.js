6b5d7846d197c68243663dd5dddea0215b744c66
VeirryAu <veirryau@gmail.com>
2023-03-15 14:10:38 +0700

    prevent press enter on payment

--- src/routes/transaction/pos/payment/ModalPaymentConfirm.js
+++ src/routes/transaction/pos/payment/ModalPaymentConfirm.js
@@ -8,15 +8,41 @@ class ModalPaymentConfirm extends Component {
     return (
       <Modal
         {...modalProps}
-        onOk={() => {
+        onOk={(e) => {
+          e.preventDefault()
+          const selector = document.getElementById('buttonCancel')
+          if (selector) {
+            selector.focus()
+            selector.select()
+          }
           onOk()
+        }}
+        onKeyDown={(e) => {
+          if (e.keyCode === 13) {
+            e.preventDefault()
+          }
         }}
         onCancel={() => {
           onCancel()
         }}
         footer={[
           (<Button id="buttonCancel" type="default" onClick={onCancel} disabled={loading.effects['payment/create']}>Cancel</Button>),
-          (<Button id="buttonOk" type="primary" onClick={onOk} disabled={loading.effects['payment/create']}>Ok</Button>)
+          (<Button
+            id="buttonOk"
+            type="primary"
+            onKeyDown={(e) => {
+              if (e.keyCode === 13) {
+                e.preventDefault()
+              }
+            }}
+            onClick={(e) => {
+              e.preventDefault()
+              onOk()
+            }}
+            disabled={loading.effects['payment/create']}
+          >
+            Ok
+          </Button>)
         ]}
       >
         <div style={{ fontWeight: 'bold', fontSize: '16px' }}>Save Payment</div>
