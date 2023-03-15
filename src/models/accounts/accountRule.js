1f93246c4c78e7ec0863e09597224edd9df4b00d
VeirryAu <veirryau@gmail.com>
2023-03-15 12:49:50 +0700

    accountRule finish

--- src/models/accounts/accountRule.js
+++ src/models/accounts/accountRule.js
@@ -2,6 +2,28 @@ import modelExtend from 'dva-model-extend'
 import { message } from 'antd'
 import { query, queryId, add } from 'services/account/accountRule'
 import { pageModel } from 'models/common'
+import pathToRegexp from 'path-to-regexp'
+import { lstorage } from 'utils'
+import {
+  ACCOUNT_TYPE_BANK,
+  ACCOUNT_TYPE_ADJUST,
+  ACCOUNT_TYPE_PETTY_CASH,
+  ACCOUNT_TYPE_PETTY_CASH_EXPENSE,
+
+  CACHE_TYPE_ALL,
+  CACHE_TYPE_BANK,
+  CACHE_TYPE_ADJUST,
+  CACHE_TYPE_PETTY_CASH,
+  CACHE_TYPE_PETTY_CASH_EXPENSE,
+  CACHE_TYPE_CASH_ENTRY_EXPENSE,
+  CACHE_TYPE_BANK_ENTRY,
+  CACHE_TYPE_VOUCHER,
+  CACHE_TYPE_PAYABLE_FORM,
+  CACHE_TYPE_PAYABLE_FORM_EXPENSE,
+  CACHE_TYPE_MARKETING_VOUCHER_DETAIL,
+  CACHE_TYPE_TRANSFER_INVOICE,
+  CACHE_TYPE_MASTER_ACCOUNT
+} from 'utils/variable'
 
 const success = () => {
   message.success('Account Code has been saved')
@@ -14,7 +36,9 @@ export default modelExtend(pageModel, {
     currentItem: {},
     modalType: 'add',
     activeKey: '0',
-    list: [],
+    listAccountCode: [],
+    listAccountCodeExpense: [],
+    listAccountCodeLov: [],
     pagination: {
       showSizeChanger: true,
       showQuickJumper: true,
@@ -22,7 +46,148 @@ export default modelExtend(pageModel, {
     }
   },
 
-  subscriptions: {},
+  subscriptions: {
+    setup ({ dispatch, history }) {
+      history.listen((location) => {
+        const { pathname } = location
+        const { activeKey } = location.query
+        if (pathname === '/transaction/adjust') {
+          dispatch({
+            type: 'query',
+            payload: {
+              accountType: ACCOUNT_TYPE_ADJUST,
+              cacheType: CACHE_TYPE_ADJUST
+            }
+          })
+        }
+        if (pathname === '/balance/finance/petty-expense') {
+          dispatch({
+            type: 'query',
+            payload: {
+              accountType: ACCOUNT_TYPE_PETTY_CASH,
+              cacheType: CACHE_TYPE_PETTY_CASH
+            }
+          })
+          dispatch({
+            type: 'queryExpense',
+            payload: {
+              accountType: ACCOUNT_TYPE_PETTY_CASH_EXPENSE,
+              cacheType: CACHE_TYPE_PETTY_CASH_EXPENSE
+            }
+          })
+        }
+        const matchEdc = pathToRegexp('/master/paymentoption/edc/:id').exec(pathname)
+        if (pathname === '/cash-entry'
+          || pathname === '/transfer-entry'
+          || pathname === '/bank-recon'
+          || pathname === '/balance/finance/petty-cash'
+          || pathname === '/balance/finance/history'
+          || pathname === '/master/paymentoption'
+          || matchEdc) {
+          dispatch({
+            type: 'query',
+            payload: {
+              accountType: ACCOUNT_TYPE_BANK,
+              cacheType: CACHE_TYPE_BANK
+            }
+          })
+          dispatch({
+            type: 'queryExpense',
+            payload: {
+              accountType: ACCOUNT_TYPE_PETTY_CASH_EXPENSE,
+              cacheType: CACHE_TYPE_CASH_ENTRY_EXPENSE
+            }
+          })
+        }
+        if (pathname === '/report/accounting/consolidation/general-ledger'
+          || pathname === '/bank-history'
+          || pathname === '/tools/report/general-ledger') {
+          dispatch({
+            type: 'query',
+            payload: {
+              accountType: [],
+              cacheType: CACHE_TYPE_ALL
+            }
+          })
+        }
+
+        if (pathname === '/bank-entry') {
+          dispatch({
+            type: 'query',
+            payload: {
+              accountType: ACCOUNT_TYPE_BANK,
+              cacheType: CACHE_TYPE_BANK
+            }
+          })
+          dispatch({
+            type: 'queryExpense',
+            payload: {
+              accountType: [],
+              cacheType: CACHE_TYPE_BANK_ENTRY
+            }
+          })
+        }
+        if (pathname === '/inventory/transfer/invoice') {
+          dispatch({
+            type: 'query',
+            payload: {
+              accountType: ACCOUNT_TYPE_BANK,
+              cacheType: CACHE_TYPE_TRANSFER_INVOICE
+            }
+          })
+        }
+        if (pathname === '/marketing/voucher') {
+          dispatch({
+            type: 'query',
+            payload: {
+              accountType: ['REVE', 'OINC'],
+              cacheType: CACHE_TYPE_VOUCHER
+            }
+          })
+        }
+        if (pathname === '/accounts/payable-form') {
+          dispatch({
+            type: 'query',
+            payload: {
+              accountType: [],
+              cacheType: CACHE_TYPE_PAYABLE_FORM
+            }
+          })
+          dispatch({
+            type: 'queryLov',
+            payload: {
+              accountType: ACCOUNT_TYPE_BANK,
+              cacheType: CACHE_TYPE_PAYABLE_FORM_EXPENSE
+            }
+          })
+        }
+        const matchVoucher = pathToRegexp('/marketing/voucher/:id').exec(pathname)
+        if (matchVoucher) {
+          dispatch({
+            type: 'queryLov',
+            payload: {
+              accountType: ACCOUNT_TYPE_BANK,
+              cacheType: CACHE_TYPE_MARKETING_VOUCHER_DETAIL
+            }
+          })
+        }
+        if (
+          pathname === '/master/account'
+          || pathname === '/journal-entry'
+          || pathname === '/tools/transaction/journal-entry') {
+          if (activeKey !== '1') {
+            dispatch({
+              type: 'queryLov',
+              payload: {
+                accountType: [],
+                cacheType: CACHE_TYPE_MASTER_ACCOUNT
+              }
+            })
+          }
+        }
+      })
+    }
+  },
 
   effects: {
     * queryId ({ payload = {} }, { call, put }) {
@@ -40,17 +205,52 @@ export default modelExtend(pageModel, {
     },
 
     * query ({ payload = {} }, { call, put }) {
-      const response = yield call(query, payload)
+      const response = yield call(query, {
+        storeId: lstorage.getCurrentUserStore(),
+        userRole: lstorage.getCurrentUserRole(),
+        ...payload
+      })
       if (response.success) {
         yield put({
           type: 'querySuccess',
           payload: {
-            list: response.data,
-            pagination: {
-              current: Number(response.page) || 1,
-              pageSize: Number(response.pageSize) || 10,
-              total: response.total
-            }
+            listAccountCode: response.data
+          }
+        })
+      } else {
+        throw response
+      }
+    },
+
+    * queryExpense ({ payload = {} }, { call, put }) {
+      const response = yield call(query, {
+        storeId: lstorage.getCurrentUserStore(),
+        userRole: lstorage.getCurrentUserRole(),
+        ...payload
+      })
+      if (response.success) {
+        yield put({
+          type: 'querySuccess',
+          payload: {
+            listAccountCodeExpense: response.data
+          }
+        })
+      } else {
+        throw response
+      }
+    },
+
+    * queryLov ({ payload = {} }, { call, put }) {
+      const response = yield call(query, {
+        storeId: lstorage.getCurrentUserStore(),
+        userRole: lstorage.getCurrentUserRole(),
+        ...payload
+      })
+      if (response.success) {
+        yield put({
+          type: 'querySuccess',
+          payload: {
+            listAccountCodeLov: response.data
           }
         })
       } else {
