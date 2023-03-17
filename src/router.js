b5c2d7f8f6b8b0b28e81db9f9d5381ec9a0bcf85
VeirryAu <veirryau@gmail.com>
2023-03-17 12:36:45 +0700

    missing accountRule

--- src/router.js
+++ src/router.js
@@ -383,6 +383,7 @@ const Routers = function ({ history, app }) {
               registerModel(app, require('./models/setting/userStore'))
               registerModel(app, require('./models/setting/userRole'))
               registerModel(app, require('./models/misc'))
+              registerModel(app, require('./models/accounts/accountRule'))
               registerModel(app, require('./models/master/accountCode'))
               registerModel(app, require('./models/master/accountCodeDefault'))
               cb(null, require('./routes/master/accountCode/'))
