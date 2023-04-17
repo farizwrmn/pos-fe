2494833df8937c9a76c2623c6783fd06651eed35
dariokhory <dariokho@gmail.com>
2023-04-17 11:09:43 +0700

    add payment machine store model

---
+++ src/models/master/paymentOption/store.js
@@ -0,0 +1,68 @@
+import modelExtend from 'dva-model-extend'
+import { query } from 'services/master/paymentOption/paymentMachineStoreService'
+import { pageModel } from 'common'
+import { message } from 'antd'
+
+export default modelExtend(pageModel, {
+  namespace: 'paymentMachineStore',
+
+  state: {
+    activeKey: '0',
+    list: [],
+    pagination: {
+      showSizeChanger: true,
+      showQuickJumper: true,
+      current: 1
+    }
+  },
+
+  subscriptions: {
+    setup ({ dispatch, history }) {
+      history.listen((location) => {
+        const { activeKey } = location.query
+        const { pathname } = location
+        if (pathname === '/master/paymentoption') {
+          if (activeKey === '2') {
+            dispatch({
+              type: 'query',
+              payload: {}
+            })
+          }
+        }
+      })
+    }
+  },
+
+  effects: {
+
+    * query ({ payload = {} }, { call, put }) {
+      const response = yield call(query, payload)
+      if (response && response.success && response.meta) {
+        yield put({
+          type: 'querySuccess',
+          payload: {
+            list: response.data,
+            pagination: {
+              showSizeChanger: true,
+              showQuickJumper: true,
+              current: Number(payload.page || 1),
+              pageSize: Number(payload.pageSize || 10),
+              count: Number(response.meta.count || 1)
+            }
+          }
+        })
+      } else {
+        message.error(`${response.message} - Failed to get data`)
+      }
+    }
+  },
+
+  reducers: {
+    querySuccess (state, action) {
+      return {
+        ...state,
+        ...action.payload
+      }
+    }
+  }
+})
