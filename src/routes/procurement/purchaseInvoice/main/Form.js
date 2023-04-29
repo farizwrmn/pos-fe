d83a2be5922b6f3bb293f047e286585d894e9990
VeirryAu <veirryau@gmail.com>
2023-04-29 10:42:12 +0700

    addProduct hide button

--- src/routes/procurement/purchaseInvoice/main/Form.js
+++ src/routes/procurement/purchaseInvoice/main/Form.js
@@ -345,7 +345,7 @@ const FormCounter = ({
           </FormItem>
         </Col>
       </Row>
-      <Button type="primary" size="large" onClick={() => showModalReceive()} style={{ marginRight: '10px' }}>Receive</Button>
+      {item && !item.addProduct && <Button type="primary" size="large" onClick={() => showModalReceive()} style={{ marginRight: '10px' }}>Receive</Button>}
       {item && !item.receiveItem && <Button type="default" size="large" onClick={() => showModalProduct()}>Product</Button>}
       <ListItem {...listItemProps} rounding={getFieldValue('rounding') || 0} deliveryFee={getFieldValue('deliveryFee') || 0} onModalVisible={record => onShowModal(record)} style={{ marginTop: '10px' }} />
       <Button type="primary" onClick={handleSubmit} style={{ float: 'right', marginTop: '10px' }}>Save</Button>
