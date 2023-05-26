5e6a53b6eca3e96a3a3656af813efb42b2e00386
VeirryAu <veirryau@gmail.com>
2023-05-26 15:10:20 +0700

    fix data profile

--- src/models/login.js
+++ src/models/login.js
@@ -67,9 +67,8 @@ export default {
       const { data } = payload
       if (data.profile.role === 'OWN'
         || data.profile.role === 'SPR'
-        || data.permissions.role === 'HFC'
-        || data.permissions.role === 'SFC'
-        || data.profile.role === 'ADM') {
+        || data.profile.role === 'HFC'
+        || data.profile.role === 'SFC') {
         yield put({
           type: 'updateState',
           payload: {
