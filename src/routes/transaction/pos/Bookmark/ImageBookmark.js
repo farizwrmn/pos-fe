bb453ca1f234609d63331e5058afdec1508d1234
VeirryAu <veirryau@gmail.com>
2023-05-29 10:30:08 +0700

    returnPolicy for supplier

--- src/routes/transaction/pos/Bookmark/ImageBookmark.js
+++ src/routes/transaction/pos/Bookmark/ImageBookmark.js
@@ -3,6 +3,19 @@ import { IMAGEURL } from 'utils/config.company'
 import { withoutFormat } from 'utils/string'
 
 const ImageBookmark = ({ item }) => {
+  if (item && item.bookmarkImage) {
+    if (item
+      && item != null
+      && item.bookmarkImage != null
+      && item.bookmarkImage !== '["no_image.png"]'
+      && item.bookmarkImage !== '"no_image.png"'
+      && item.bookmarkImage !== 'no_image.png') {
+      if (item.bookmarkImage) {
+        return <img height="70px" src={`${IMAGEURL}/${withoutFormat(item.bookmarkImage)}-cropped.jpg`} alt="no_image" />
+      }
+    }
+  }
+
   if (item && item.product) {
     if (item.product
       && item.product != null
@@ -12,7 +25,7 @@ const ImageBookmark = ({ item }) => {
       && item.product.productImage !== 'no_image.png') {
       const bookmarkImage = JSON.parse(item.product.productImage)
       if (bookmarkImage && bookmarkImage[0]) {
-        return <img height="70px" src={`${IMAGEURL}/${withoutFormat(bookmarkImage[0])}-main.jpg`} alt="no_image" />
+        return <img height="70px" src={`${IMAGEURL}/${withoutFormat(bookmarkImage[0])}-cropped.jpg`} alt="no_image" />
       }
     }
   }
@@ -26,7 +39,7 @@ const ImageBookmark = ({ item }) => {
       && item.bundle.productImage !== 'no_image.png') {
       const bookmarkImage = JSON.parse(item.bundle.productImage)
       if (bookmarkImage && bookmarkImage[0]) {
-        return <img height="70px" src={`${IMAGEURL}/${withoutFormat(bookmarkImage[0])}-main.jpg`} alt="no_image" />
+        return <img height="70px" src={`${IMAGEURL}/${withoutFormat(bookmarkImage[0])}-cropped.jpg`} alt="no_image" />
       }
     }
   }
