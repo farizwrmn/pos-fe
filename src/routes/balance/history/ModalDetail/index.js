5a39d65e0fecddc6d5862c719bb994e2dca4c63a
VeirryAu <veirryau@gmail.com>
2023-06-14 16:04:21 +0700

    balance history

--- src/routes/balance/history/ModalDetail/index.js
+++ src/routes/balance/history/ModalDetail/index.js
@@ -4,25 +4,30 @@ import BalanceUser from './BalanceUser'
 import BalanceSummary from './BalanceSummary'
 import BalanceDetail from './BalanceDetail'
 
-const ModalApprove = ({ ...modalProps }) => {
+const ModalApprove = (modalProps) => {
+  const { item } = modalProps
   return (
     <div>
       <Modal {...modalProps}>
         <Card title="User Info">
           <BalanceUser {...modalProps} />
         </Card>
-        <Card
-          title={(
-            <div>
-              <div>{'Problem Summary (negative => Overinput)'}</div>
-            </div>
-          )}
-        >
-          <BalanceSummary {...modalProps} />
-        </Card>
-        <Card title="Detail (Closing - By input)">
-          <BalanceDetail {...modalProps} />
-        </Card>
+        {item && item.closed ? (
+          <Card
+            title={(
+              <div>
+                <div>{'Problem Summary (negative => Overinput)'}</div>
+              </div>
+            )}
+          >
+            <BalanceSummary {...modalProps} />
+          </Card>
+        ) : null}
+        {item && item.closed ? (
+          <Card title="Detail (Closing - By input)">
+            <BalanceDetail {...modalProps} />
+          </Card>
+        ) : null}
       </Modal>
     </div>
   )
