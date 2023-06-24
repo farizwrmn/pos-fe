f163ebe2b20432bd3028c0d56bc6c69bef9b13e6
VeirryAu <veirryau@gmail.com>
2023-06-24 15:08:59 +0700

    remove unused component

--- src/routes/k3express/kiosk/kioskCategory/index.js
+++ src/routes/k3express/kiosk/kioskCategory/index.js
@@ -4,64 +4,11 @@ import { connect } from 'dva'
 import { routerRedux } from 'dva/router'
 import { Button, Tabs } from 'antd'
 import Form from './Form'
-import List from './List'
-import Filter from './Filter'
 
 const TabPane = Tabs.TabPane
 
-const Counter = ({ kioskCategory, loading, dispatch, location, app }) => {
-  const { list, pagination, modalType, currentItem, activeKey } = kioskCategory
-  const { user, storeInfo } = app
-  const filterProps = {
-    onFilterChange (value) {
-      dispatch({
-        type: 'kioskCategory/query',
-        payload: {
-          ...value
-        }
-      })
-    }
-  }
-
-  const listProps = {
-    dataSource: list,
-    user,
-    storeInfo,
-    pagination,
-    loading: loading.effects['kioskCategory/query'],
-    location,
-    onChange (page) {
-      const { query, pathname } = location
-      dispatch(routerRedux.push({
-        pathname,
-        query: {
-          ...query,
-          page: page.current,
-          pageSize: page.pageSize
-        }
-      }))
-    },
-    editItem (item) {
-      const { pathname } = location
-      dispatch(routerRedux.push({
-        pathname,
-        query: {
-          activeKey: 0
-        }
-      }))
-      dispatch({
-        type: 'kioskCategory/editItem',
-        payload: { item }
-      })
-    },
-    deleteItem (id) {
-      dispatch({
-        type: 'kioskCategory/delete',
-        payload: id
-      })
-    }
-  }
-
+const Counter = ({ kioskCategory, dispatch, location }) => {
+  const { modalType, currentItem, activeKey } = kioskCategory
   const changeTab = (key) => {
     dispatch({
       type: 'kioskCategory/changeTab',
@@ -127,14 +74,6 @@ const Counter = ({ kioskCategory, loading, dispatch, location, app }) => {
       <Tabs activeKey={activeKey} onChange={key => changeTab(key)} tabBarExtraContent={moreButtonTab} type="card">
         <TabPane tab="Form" key="0" >
           {activeKey === '0' && <Form {...formProps} />}
-        </TabPane>
-        <TabPane tab="Browse" key="1" >
-          {activeKey === '1' &&
-            <div>
-              <Filter {...filterProps} />
-              <List {...listProps} />
-            </div>
-          }
         </TabPane>
       </Tabs>
     </div>
