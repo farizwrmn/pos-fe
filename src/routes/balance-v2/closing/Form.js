f0f3ab5acd56c3a394bc1ecaa317adfec8ea6170
dariokhory <dariokho@gmail.com>
2023-08-05 15:35:34 +0700

    feat(api): update UI

--- src/routes/balance-v2/closing/Form.js
+++ src/routes/balance-v2/closing/Form.js
@@ -1,9 +1,39 @@
-const FormClosing = () => {
+import { Col, Form, Input } from 'antd'
+
+const FormItem = Form.Item
+const formItemLayout = {
+  labelCol: {
+    xs: 8,
+    sm: 8,
+    md: 4,
+    lg: 4,
+    xl: 2
+  },
+  wrapperCol: {
+    xs: 16,
+    sm: 16,
+    md: 8,
+    lg: 8,
+    xl: 4
+  }
+}
+
+const FormClosing = ({
+  form: {
+    getFieldDecorator
+  }
+}) => {
   return (
-    <div>
-      Test
-    </div>
+    <Col span={24}>
+      <Form layout="horizontal">
+        <FormItem label="name" hasFeedback {...formItemLayout}>
+          {getFieldDecorator('name')(
+            <Input />
+          )}
+        </FormItem>
+      </Form>
+    </Col>
   )
 }
 
-export default FormClosing
+export default Form.create()(FormClosing)
