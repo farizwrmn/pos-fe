e9c96c64d34202d1badc1395bccb92ba72ed126c
Dario Khory <dariokho@gmail.com>
2023-09-07 08:53:41 +0700

    feat(ui): list detail journal footer

--- src/routes/depositCashier/FormDeposit/utils/ListJournal.js
+++ src/routes/depositCashier/FormDeposit/utils/ListJournal.js
@@ -1,9 +1,11 @@
-import { Table } from 'antd'
+import { Col, Row, Table } from 'antd'
 import { currencyFormatter } from 'utils/string'
 
 const ListJournal = ({
   ...tableProps
 }) => {
+  const { dataSource } = tableProps
+
   const columns = [
     {
       title: 'Account',
@@ -34,6 +36,30 @@ const ListJournal = ({
       {...tableProps}
       columns={columns}
       bordered
+      footer={() => {
+        const totalDebit = dataSource.reduce((prev, curr) => { return prev + curr.amountIn }, 0)
+        const totalCredit = dataSource.reduce((prev, curr) => { return prev + curr.amountOut }, 0)
+        return (
+          <Col>
+            <Row type="flex">
+              <Col>
+                Debit
+              </Col>
+              <Col>
+                {currencyFormatter(totalDebit)}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Credit
+              </Col>
+              <Col>
+                {currencyFormatter(totalCredit)}
+              </Col>
+            </Row>
+          </Col>
+        )
+      }}
     />
   )
 }
