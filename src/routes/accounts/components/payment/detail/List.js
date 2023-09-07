9c275faf3f429314f8a272f8125179dcd31346a9
VeirryAu <veirryau@gmail.com>
2023-09-07 11:03:27 +0700

    lint: fix new line

--- src/routes/accounts/components/payment/detail/List.js
+++ src/routes/accounts/components/payment/detail/List.js
@@ -36,12 +36,13 @@ const List = ({ cancelPayment, ...tableProps }) => {
       dataIndex: 'active',
       key: 'active',
       width: 120,
-      render: text =>
-      (<span>
-        <Tag color={parseInt(text, 10) ? 'blue' : 'red'}>
-          {parseInt(text, 10) ? 'Active' : 'Canceled'}
-        </Tag>
-      </span>)
+      render: text => (
+        <span>
+          <Tag color={parseInt(text, 10) ? 'blue' : 'red'}>
+            {parseInt(text, 10) ? 'Active' : 'Canceled'}
+          </Tag>
+        </span>
+      )
     },
     {
       title: 'Date',
