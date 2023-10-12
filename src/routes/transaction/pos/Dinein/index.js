ddac3324f5aaabcd8f6af995572b6a829556acab
vendrieou <vendrieok@gmail.com>
2023-10-12 17:31:12 +0700

    feature: k3express DineinMap

---
+++ src/routes/transaction/pos/Dinein/index.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import { Modal } from 'antd'
+import FormEdit from './FormEdit'
+import List from './List'
+
+const Express = ({
+  visible,
+  editVisible,
+  loading,
+  item,
+  list,
+  className,
+  onClose,
+  onCloseEditVisible,
+  onEditItem,
+  onEdit,
+  ...tableProps
+}) => {
+  const formEditProps = {
+    item,
+    loading,
+    editVisible,
+    onCloseEditVisible,
+    onEdit
+  }
+
+  const listProps = {
+    dataSource: list,
+    onEditItem
+  }
+
+  return (
+    <div>
+      {editVisible && <FormEdit {...formEditProps} />}
+      {visible ?
+        <Modal
+          className={className}
+          visible={visible}
+          onCancel={onClose}
+          title="Produk K3 Express"
+          width="80%"
+          footer={null}
+          {...tableProps}
+        >
+          <List {...listProps} />
+        </Modal> : null}
+    </div>
+  )
+}
+
+export default Express
