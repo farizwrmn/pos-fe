6fd1050fe8ae3b9ba2c09e627d8c3a0b068a07ec
vendrieou <vendrieok@gmail.com>
2024-05-27 16:13:38 +0700

    update setoran

--- src/routes/balance/closing/ConfirmationDialog.js
+++ src/routes/balance/closing/ConfirmationDialog.js
@@ -13,38 +13,12 @@ const column = {
 }
 
 const ConfirmationDialog = ({
-  dispatch,
   list = [],
   listSetoran = [],
-  setCashValue,
   form: {
-    // getFieldsValue,
     getFieldDecorator
   }
 }) => {
-  const onChangeInput = (itemSelection, value) => {
-    let currentList = list && list.length > 0 && list.map((item) => {
-      if (item.name === itemSelection.name && item.type === itemSelection.type) {
-        return {
-          ...item,
-          physicalMoneyId: itemSelection.id,
-          qty: value,
-          amount: itemSelection.value * value
-        }
-      }
-      return item
-    })
-
-    dispatch({
-      type: 'physicalMoney/updateState',
-      payload: {
-        list: currentList
-      }
-    })
-    let amount = currentList && currentList.length > 0 && currentList.reduce((cnt, o) => cnt + parseFloat(o.amount || 0), 0)
-    setCashValue(amount)
-  }
-
   const filterList = list.filter(fitlered => !!fitlered.amount)
   const tableProps = {
     pagination: false,
@@ -53,10 +27,12 @@ const ConfirmationDialog = ({
   }
 
   const defaultList = [{ total: 0, qty: 0 }]
-  const filterListEdc = listSetoran && listSetoran.length > 0 && listSetoran.filter(filtered => filtered.status === 'A')
-  const filterListVoid = listSetoran && listSetoran.length > 0 && listSetoran.filter(filtered => filtered.status === 'C')
+  const filterListEdc = (listSetoran && listSetoran.length > 0 && listSetoran.filter(filtered => filtered.status === 'A')) || []
+  const filterListVoid = (listSetoran && listSetoran.length > 0 && listSetoran.filter(filtered => filtered.status === 'C')) || []
+  const filterListGrab = (listSetoran && listSetoran.length > 0 && listSetoran.filter(filtered => filtered.status === 'G')) || []
   const listEdc = filterListEdc && filterListEdc.length > 0 ? filterListEdc : defaultList
   const listVoid = filterListVoid && filterListVoid.length > 0 ? filterListVoid : defaultList
+  const listGrab = filterListGrab && filterListGrab.length > 0 ? filterListGrab : defaultList
 
   const listEdcProps = {
     pagination: false,
@@ -70,12 +46,20 @@ const ConfirmationDialog = ({
     dataSource: listVoid
   }
 
-  let itemA = listSetoran && listSetoran.find(item => item.status === 'A')
+  const listGrabProps = {
+    pagination: false,
+    defaultExpandAllRows: true,
+    dataSource: listGrab
+  }
+
+  let itemA = (listSetoran && listSetoran.find(item => item.status === 'A')) || {}
   let edcAmount = itemA && itemA.edcAmount ? itemA.edcAmount : 0
   let edcTotal = itemA && itemA.edcTotal ? itemA.edcTotal : 0
-  let itemB = listSetoran && listSetoran.find(item => item.status === 'C')
+  let itemB = (listSetoran && listSetoran.find(item => item.status === 'C')) || {}
   let voidAmount = itemB && itemB.voidAmount ? itemB.voidAmount : 0
   let voidTotal = itemB && itemB.voidTotal ? itemB.voidTotal : 0
+  let itemC = (listSetoran && listSetoran.find(item => item.status === 'G')) || {}
+  let grabAmount = itemC && itemC.grabAmount ? itemC.grabAmount : 0
   let subtotalValue = (list || [])
     .filter(filtered => typeof filtered.amount === 'number' && !isNaN(filtered.amount))
     .reduce((cnt, o) => cnt + parseFloat(o.amount), 0)
@@ -95,7 +79,7 @@ const ConfirmationDialog = ({
                   <FormItem hasFeedback>
                     {getFieldDecorator(`${column.name}-${column.type}`, {
                       initialValue: column && column.qty ? column.qty : 0
-                    })(<InputNumber disabled min={0} onChange={value => onChangeInput(column, value)} />)}
+                    })(<InputNumber disabled min={0} />)}
                   </FormItem>
                 ) : null}
               </div>
@@ -220,7 +204,6 @@ const ConfirmationDialog = ({
             </div>
           </Col>
           <Col>
-            {/* <ListVoid {...listVoidProps} /> */}
             <div>
               <h3 style={{ fontWeight: 'bold' }}>Struk Void / Cancel</h3>
               <Table {...listVoidProps}>
@@ -259,16 +242,6 @@ const ConfirmationDialog = ({
                   )}
                 />
               </Table>
-              {/* <Column
-                    title="TOTAL"
-                    dataIndex="total"
-                    key="total"
-                    render={(text, column) => (
-                      <div style={{ textAlign: 'center' }}>
-                        <p>{currencyFormatterSetoran(column.total)}</p>
-                      </div>
-                    )}
-                  /> */}
               <Row style={{ padding: '1em' }}>
                 <Col span={18} style={{ textAlign: 'center' }}>
                   <p style={{ fontWeight: 'bold' }}>Subtotal</p>
@@ -282,6 +255,28 @@ const ConfirmationDialog = ({
             </div>
           </Col>
         </Row>
+      </Col>
+      <Col>
+        <div>
+          <h3 style={{ fontWeight: 'bold' }}>Struk Grabmart</h3>
+          <Table {...listGrabProps}>
+            <Column
+              title="JUMLAH LEMBAR"
+              dataIndex="grabAmount"
+              key="grabAmount"
+              render={() => (
+                <div style={{ textAlign: 'center' }}>
+                  <FormItem hasFeedback>
+                    {getFieldDecorator('grabAmount', {
+                      initialValue: grabAmount
+                    })(<InputNumber disabled style={{ width: 200 }} min={0} />)}
+                  </FormItem>
+                </div>
+              )}
+            />
+          </Table>
+
+        </div>
       </Col>
     </Row>
   )
