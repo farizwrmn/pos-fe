b954fc5d1170f62f7a8e539f4bce6c9b8a4dba9e
VeirryAu <veirryau@gmail.com>
2025-02-04 16:01:23 +0700

    Remove mindis

--- src/routes/inventory/transfer/autoReplenish/Form.js
+++ src/routes/inventory/transfer/autoReplenish/Form.js
@@ -31,7 +31,7 @@ const column = {
 
 const FormCounter = ({
   item = {},
-  onSubmit,
+  // onSubmit,
   onSubmitPkm,
   modalType,
   listStore,
@@ -63,31 +63,31 @@ const FormCounter = ({
     }
   }
 
-  const handleSubmit = () => {
-    validateFields((errors) => {
-      if (errors) {
-        return
-      }
-      const data = {
-        ...getFieldsValue()
-      }
-      Modal.confirm({
-        title: 'Do you want to save this item?',
-        onOk () {
-          onSubmit({
-            header: {
-              pickingLineId: data.pickingLineId,
-              storeIdReceiver: data.storeIdReceiver,
-              storeId: lstorage.getCurrentUserStore(),
-              salesDateFrom: data.salesDate ? data.salesDate[0].format('YYYY-MM-DD') : undefined,
-              salesDateTo: data.salesDate ? data.salesDate[1].format('YYYY-MM-DD') : undefined
-            }
-          }, resetFields)
-        },
-        onCancel () { }
-      })
-    })
-  }
+  // const handleSubmit = () => {
+  //   validateFields((errors) => {
+  //     if (errors) {
+  //       return
+  //     }
+  //     const data = {
+  //       ...getFieldsValue()
+  //     }
+  //     Modal.confirm({
+  //       title: 'Do you want to save this item?',
+  //       onOk () {
+  //         onSubmit({
+  //           header: {
+  //             pickingLineId: data.pickingLineId,
+  //             storeIdReceiver: data.storeIdReceiver,
+  //             storeId: lstorage.getCurrentUserStore(),
+  //             salesDateFrom: data.salesDate ? data.salesDate[0].format('YYYY-MM-DD') : undefined,
+  //             salesDateTo: data.salesDate ? data.salesDate[1].format('YYYY-MM-DD') : undefined
+  //           }
+  //         }, resetFields)
+  //       },
+  //       onCancel () { }
+  //     })
+  //   })
+  // }
 
   const handleSubmitPkm = () => {
     validateFields((errors) => {
