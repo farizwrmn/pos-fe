26332c978cb7f8e4499b0d995e317bde6f9ad6f3
VeirryAu <veirryau@gmail.com>
2025-03-21 14:58:10 +0700

    fix eslint error

--- src/routes/transaction/pos/NotFoundPage.js
+++ src/routes/transaction/pos/NotFoundPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Icon } from 'antd'
+import styles from '../../error/index.less'
 
-const NotFoundPage = () => {
-  return (
-    <div>404 Not Found</div>
-  )
-}
+const Error = () => (<div className="content-inner">
+  <div className={styles.error}>
+    <Icon type="frown-o" />
+    <h1>404 Not Found</h1>
+  </div>
+</div>)
 
-export default NotFoundPage
+export default Error
