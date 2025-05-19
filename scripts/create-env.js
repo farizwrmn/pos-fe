3dc569750f1bfcffcd91bfca502e946ea4a1a96e
VeirryAu <veirryau@gmail.com>
2025-05-19 08:47:35 +0700

    fix API create env

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
 fs.writeFileSync('./.env', `
-API_ENDPOINT=${process.env.API_ENDPOINT || 'pos.k3mart.id'}
-API_ENDPOINT_ALT=${process.env.API_ENDPOINT || 'api-pos.k3mart.id'}
-API_CONSIGNMENT_ENDPOINT=${process.env.API_CONSIGNMENT_ENDPOINT || 'consignment-api.k3mart.id'}
+API_ENDPOINT=${process.env.API_ENDPOINT || 'pos.k3martpos.com'}
+API_ENDPOINT_ALT=${process.env.API_ENDPOINT || 'pos.k3martpos.com'}
+API_CONSIGNMENT_ENDPOINT=${process.env.API_CONSIGNMENT_ENDPOINT || 'consignment-api.k3martpos.com'}
 MAIN_WEBSITE=${process.env.MAIN_WEBSITE || 'k3mart.id'}
 APP_NAME=${process.env.APP_NAME || 'k3mart'}
-API_ELECTRON_ENDPOINT=${process.env.API_ELECTRON_ENDPOINT || 'localhost'}
+API_ELECTRON_ENDPOINT=${process.env.API_ELECTRON_ENDPOINT || 'http://localhost:3300'}
 `)
 
