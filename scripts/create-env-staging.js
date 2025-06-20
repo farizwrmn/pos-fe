943b2a08c7fc0bb408002ace541423c7eec033c5
VeirryAu <veirryau@gmail.com>
2025-06-20 14:51:24 +0700

    fix api pos

--- scripts/create-env-staging.js
+++ scripts/create-env-staging.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
 fs.writeFileSync('./.env', `
-API_ENDPOINT=${process.env.API_ENDPOINT || 'pos.k3martpos.com'}
-API_ENDPOINT_ALT=${process.env.API_ENDPOINT || 'pos.k3martpos.com'}
+API_ENDPOINT=${process.env.API_ENDPOINT || 'api-pos.k3martpos.com'}
+API_ENDPOINT_ALT=${process.env.API_ENDPOINT || 'api-pos.k3martpos.com'}
 API_CONSIGNMENT_ENDPOINT=${process.env.API_CONSIGNMENT_ENDPOINT || 'consignment-api.k3martpos.com'}
 MAIN_WEBSITE=${process.env.MAIN_WEBSITE || 'k3mart.id'}
 APP_NAME=${process.env.APP_NAME || 'k3mart'}
--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,11 +1,10 @@
 const fs = require('fs')
 
 fs.writeFileSync('./.env', `
-API_ENDPOINT=${process.env.API_ENDPOINT || 'pos.k3martpos.com'}
-API_ENDPOINT_ALT=${process.env.API_ENDPOINT || 'pos.k3martpos.com'}
+API_ENDPOINT=${process.env.API_ENDPOINT || 'api-pos.k3martpos.com'}
+API_ENDPOINT_ALT=${process.env.API_ENDPOINT || 'api-pos.k3martpos.com'}
 API_CONSIGNMENT_ENDPOINT=${process.env.API_CONSIGNMENT_ENDPOINT || 'consignment-api.k3martpos.com'}
 MAIN_WEBSITE=${process.env.MAIN_WEBSITE || 'k3mart.id'}
 APP_NAME=${process.env.APP_NAME || 'k3mart'}
 API_ELECTRON_ENDPOINT=${process.env.API_ELECTRON_ENDPOINT || 'http://localhost:3300'}
 `)
-
